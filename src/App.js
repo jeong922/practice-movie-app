ea65e22839309ffb6500eb3d86acf908ed89b610
jeong922 <hwajeong922@naver.com>
2021-12-14 17:20:47 +0900

    app.js modify(3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
